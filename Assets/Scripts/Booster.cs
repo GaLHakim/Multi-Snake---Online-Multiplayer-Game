c9df6dbe32f971dd692339624d08e3c45f3eea90
GaLHakim <galihalhakim15@gmail.com>
2019-05-09 09:37:37 +0700

    Upload Project

---
+++ Assets/Scripts/Booster.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Booster : MonoBehaviour
+{
+    public float boostTime = 2.0f;
+    public float currentBoostTime;
+    public float boostDelayTime = 5.0f;
+    public float currentBoostDelayTime;
+    public bool boosting = false;
+    public float time;
+
+    public float baseSpeed = 1.0f;
+    public float speedBoost = 2.0f;
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentBoostTime = 0f;
+        currentBoostDelayTime = 0f;
+        speed = baseSpeed;
+    }
+
+    void movePlayer()
+    {
+        if (Input.GetKeyDown(KeyCode.Q) && !boosting && Time.time > currentBoostDelayTime)
+        {
+            currentBoostTime = Time.time + boostTime;
+            boosting = true;
+        }
+
+        if ((Time.time > currentBoostTime) && boosting)
+        {
+            boosting = false;
+            currentBoostDelayTime = Time.time + boostDelayTime;
+        }
+
+        if (boosting)
+        {
+            speed = speedBoost;
+
+        }
+
+        if (!boosting)
+        {
+            speed = baseSpeed;
+
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        time = Time.time;
+        movePlayer();
+    }
+}
---
+++ Assets/Scripts/MovePlayers.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayers : MonoBehaviour {
+
+    public Booster moveSpeed;
+	public VJHandler jsMovement;
+	
+	private Vector3 direction;
+	private float xMin,xMax,yMin,yMax;
+	
+	void Update () {
+		
+		direction = jsMovement.InputDirection;
+		
+		if(direction.magnitude != 0){
+		
+			transform.position += direction * moveSpeed.speed;
+			transform.position = new Vector3(Mathf.Clamp(transform.position.x,xMin,xMax),Mathf.Clamp(transform.position.y,yMin,yMax),0f);
+		}	
+	}	
+	
+	void Start(){
+	
+		xMax = Screen.width - 50;
+		xMin = 50; 
+		yMax = Screen.height - 50;
+		yMin = 50;
+	}
+}
